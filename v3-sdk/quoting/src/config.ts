7e8e226b955d0de55ce8fb2fb80ccae1b9921293
Wenhan Wu <wenhan.wu@4alpha-capital.com>
2023-12-28 20:53:14 +0800

    test

--- v3-sdk/quoting/src/config.ts
+++ v3-sdk/quoting/src/config.ts
@@ -1,6 +1,6 @@
 import { Token } from "@uniswap/sdk-core";
 import { FeeAmount } from "@uniswap/v3-sdk";
-import { USDC_TOKEN, WETH_TOKEN } from "./libs/constants";
+import { USDT_TOKEN, ZOOA_TOKEN } from "./libs/constants";
 
 // Inputs that configure this example to run
 export interface ExampleConfig {
@@ -24,9 +24,9 @@ export const CurrentConfig: ExampleConfig = {
     mainnet: "https://mainnet.infura.io/v3/ccf0630254a74d5d9bd148b2681eca24"
   },
   tokens: {
-    in: USDC_TOKEN,
+    in: USDT_TOKEN,
     amountIn: 1000,
-    out: WETH_TOKEN,
+    out: ZOOA_TOKEN,
     poolFee: FeeAmount.MEDIUM
   }
 };
--- v3-sdk/quoting/src/quote.ts
+++
@@ -1,3 +0,0 @@
-import { quote } from "./libs/quote";
-
-quote().then(console.log);
