baa71919ba76d04dcddb0b3d426a89aa155946e1
Wenhan Wu <wenhan.wu@4alpha-capital.com>
2023-12-28 22:03:52 +0800

    test

--- v3-sdk/quoting/src/config.ts
+++ v3-sdk/quoting/src/config.ts
@@ -1,6 +1,6 @@
 import { Token } from "@uniswap/sdk-core";
 import { FeeAmount } from "@uniswap/v3-sdk";
-import { USDT_TOKEN, ZOOA_TOKEN } from "./libs/constants";
+import { USDT_TOKEN, WETH_TOKEN, ZOOA_TOKEN } from "./libs/constants";
 
 // Inputs that configure this example to run
 export interface ExampleConfig {
@@ -15,13 +15,14 @@ export interface ExampleConfig {
 }
 
 // Example Configuration
+USDT_TOKEN;
 
 export const CurrentConfig: ExampleConfig = {
   rpc: {
     mainnet: "https://mainnet.infura.io/v3/ccf0630254a74d5d9bd148b2681eca24"
   },
   tokens: {
-    in: USDT_TOKEN,
+    in: WETH_TOKEN,
     out: ZOOA_TOKEN,
     poolFee: FeeAmount.HIGH
   }
--- v3-sdk/quoting/src/libs/constants.ts
+++ v3-sdk/quoting/src/libs/constants.ts
@@ -5,12 +5,21 @@ import { SupportedChainId, Token } from "@uniswap/sdk-core";
 // Addresses
 
 export const POOL_FACTORY_CONTRACT_ADDRESS =
-  "0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36";
+  "0xf20F344c8E21a4D5684C96E4540DD2D7D8F1Fb0f"; // weth -> zooa
+//  "0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36";
 // "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 export const QUOTER_CONTRACT_ADDRESS =
   "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
 
 // Currencies and Tokens
+
+export const WETH_TOKEN = new Token(
+  SupportedChainId.MAINNET,
+  "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  18,
+  "WETH",
+  "Wrapped Ether"
+);
 
 export const USDT_TOKEN = new Token(
   SupportedChainId.MAINNET,
