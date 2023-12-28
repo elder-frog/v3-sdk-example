cc3ffa9d1e3c30acef6eb4754d589c6d50a7e383
Wenhan Wu <wenhan.wu@4alpha-capital.com>
2023-12-28 20:40:11 +0800

    test

--- v3-sdk/quoting/src/libs/constants.ts
+++ v3-sdk/quoting/src/libs/constants.ts
@@ -1,28 +1,28 @@
 // This file stores web3 related constants such as addresses, token definitions, ETH currency references and ABI's
 
-import { SupportedChainId, Token } from '@uniswap/sdk-core'
+import { SupportedChainId, Token } from "@uniswap/sdk-core";
 
 // Addresses
 
 export const POOL_FACTORY_CONTRACT_ADDRESS =
-  '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+  "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 export const QUOTER_CONTRACT_ADDRESS =
-  '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6'
+  "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
 
 // Currencies and Tokens
 
-export const WETH_TOKEN = new Token(
+export const USDT_TOKEN = new Token(
   SupportedChainId.MAINNET,
-  '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-  18,
-  'WETH',
-  'Wrapped Ether'
-)
-
-export const USDC_TOKEN = new Token(
-  SupportedChainId.MAINNET,
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  "0xdAC17F958D2ee523a2206206994597C13D831ec7",
   6,
-  'USDC',
-  'USD//C'
-)
+  "USDT",
+  "USDT"
+);
+
+export const ZOOA_TOKEN = new Token(
+  SupportedChainId.MAINNET,
+  "0xC256F81d35a54c3599B424171d719E9Ae87b2E9b",
+  18,
+  "ZOOA",
+  "ZOOA"
+);
