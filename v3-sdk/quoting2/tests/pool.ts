7f1437a031cf9a7528282fa11e81600a43a58ffc
Wenhan Wu <wenhan.wu@4alpha-capital.com>
2023-12-28 21:53:10 +0800

    test

--- v3-sdk/quoting2/tests/pool.ts
+++ v3-sdk/quoting2/tests/pool.ts
@@ -1,22 +1,22 @@
-import FACTORY_ABI from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
+import UniswapV3Factory from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
 import { ethers } from "ethers";
 
+import { POOL_FACTORY_CONTRACT_ADDRESS } from "../src/libs/constants";
 import { getProvider } from "../src/libs/providers";
 
 const provider = getProvider();
 
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
-
-const uniswapV3Factory = new ethers.Contract(
-  "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
-  FACTORY_ABI as any,
-  wallet
+const factoryContract = new ethers.Contract(
+  POOL_FACTORY_CONTRACT_ADDRESS,
+  UniswapV3Factory.abi,
+  // define your provider
+  provider
 );
 
-uniswapV3Factory
+factoryContract
   .getPool(
     "0xdAC17F958D2ee523a2206206994597C13D831ec7",
     "0xC256F81d35a54c3599B424171d719E9Ae87b2E9b",
-    500
+    300
   )
   .then(console.log);
