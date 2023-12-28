9e4f32a12cc99a8cd4dc8bf23fe1cb44a94c7ea5
Wenhan Wu <wenhan.wu@4alpha-capital.com>
2023-12-28 21:23:02 +0800

    test

--- v3-sdk/quoting/src/libs/constants.ts
+++ v3-sdk/quoting/src/libs/constants.ts
@@ -5,7 +5,7 @@ import { SupportedChainId, Token } from "@uniswap/sdk-core";
 // Addresses
 
 export const POOL_FACTORY_CONTRACT_ADDRESS =
-  "0x11b815efb8f581194ae79006d24e0d814b7697f6";
+  "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 export const QUOTER_CONTRACT_ADDRESS =
   "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
 
