241faa3ae88bcfec9d1ec306c075bfeed39e0633
Wenhan Wu <wenhan.wu@4alpha-capital.com>
2023-12-28 21:49:59 +0800

    test

--- v3-sdk/quoting2/tests/pool.ts
+++ v3-sdk/quoting2/tests/pool.ts
@@ -1,28 +1,22 @@
-import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
-import { computePoolAddress } from "@uniswap/v3-sdk";
+import FACTORY_ABI from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
 import { ethers } from "ethers";
 
-import { CurrentConfig } from "../src/config";
-import { POOL_FACTORY_CONTRACT_ADDRESS } from "../src/libs/constants";
 import { getProvider } from "../src/libs/providers";
 
-const currentPoolAddress = computePoolAddress({
-  factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
-  tokenA: CurrentConfig.tokens.in,
-  tokenB: CurrentConfig.tokens.out,
-  fee: CurrentConfig.tokens.poolFee
-});
+const provider = getProvider();
 
-const poolContract = new ethers.Contract(
-  currentPoolAddress,
-  IUniswapV3PoolABI.abi,
-  getProvider()
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
+
+const uniswapV3Factory = new ethers.Contract(
+  "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+  FACTORY_ABI as any,
+  wallet
 );
 
-poolContract
+uniswapV3Factory
   .getPool(
     "0xdAC17F958D2ee523a2206206994597C13D831ec7",
     "0xC256F81d35a54c3599B424171d719E9Ae87b2E9b",
-    3000
+    500
   )
   .then(console.log);
